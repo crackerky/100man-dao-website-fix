82027954749ee0510e0427fb5a6eec0d4b7b123b
crackerky <crackert.ky@gmail.com>
2025-05-30 16:56:53 +0900

    Add homepage using DemoPageTemplate with BackgroundAnimation

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { DemoPageTemplate } from '@/components'
+
+export default function HomePage() {
+  return (
+    <DemoPageTemplate 
+      title="100 Man DAO"
+      subtitle="Unity from Diversity"
+      backgroundOpacity={0.4}
+    />
+  )
+}
