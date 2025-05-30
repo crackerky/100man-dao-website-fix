7d25a33459349d449cc1a1f1b9ac17e22ab5780c
crackerky <crackert.ky@gmail.com>
2025-05-30 16:52:09 +0900

    Add components index for easy imports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Atoms
+export { default as BackgroundAnimation } from './atoms/BackgroundAnimation'
+export { default as Typography } from './atoms/Typography'
+
+// Templates  
+export { default as DemoPageTemplate } from './templates/DemoPageTemplate'
